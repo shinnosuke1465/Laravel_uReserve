3fef8db2e21b93eaedec228c866ac98a6ed7be6f
Shinnosuke1465 <s.shinno0605@icloud.com>
2023-12-08 21:32:28 +0900

    basic confirmation

---
+++ app/Http/Livewire/Counte.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counte extends Component
+{
+    public function render()
+    {
+        return view('livewire.counte');
+    }
+}
