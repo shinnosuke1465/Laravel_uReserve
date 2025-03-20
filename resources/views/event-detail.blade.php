a1506d41eb808673b0062d9378b311ff8dce92f0
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-03-20 12:19:41 +0900

    実装完了

---
+++ resources/views/event-detail.blade.php
@@ -0,0 +1,7 @@
+@auth
+    <x-event-detail-auth :event='$event' :reservablePeople='$reservablePeople' :isReserved='$isReserved' />
+@endauth
+
+@guest
+    <x-event-detail-guest :event='$event' :reservablePeople='$reservablePeople' :isReserved='$isReserved' />
+@endguest
